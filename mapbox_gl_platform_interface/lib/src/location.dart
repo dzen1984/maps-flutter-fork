ed0920aaac84c8bf945a171d1226598b32d2ce33
dzen <dzen.ivanov@gmail.com>
2025-01-18 07:50:39 +0200

    - fix

--- mapbox_gl_platform_interface/lib/src/location.dart
+++ mapbox_gl_platform_interface/lib/src/location.dart
@@ -53,7 +53,7 @@ class LatLng {
   }
 
   @override
-  int get hashCode => hashValues(latitude, longitude);
+  int get hashCode => Object.hash(latitude, longitude);
 }
 
 /// A latitude/longitude aligned rectangle.
@@ -106,7 +106,7 @@ class LatLngBounds {
   }
 
   @override
-  int get hashCode => hashValues(southwest, northeast);
+  int get hashCode => Object.hash(southwest, northeast);
 }
 
 /// A geographical area representing a non-aligned quadrilateral
